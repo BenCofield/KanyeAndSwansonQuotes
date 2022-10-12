65f8c0c1d641d190eaed170f0979fbb77e710f9d
Ben Cofield <benrcofield@gmail.com>
2022-10-12 15:25:26 -0400

    First Commit

---
+++ KanyeAndRonSwanson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRonSwanson
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new HttpClient();
+            var KanyeUrl = "https://api.kanye.rest";
+            var SwansonUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                var KanyeResponse = client.GetStringAsync(KanyeUrl).Result;
+                var KanyeQuote = JObject.Parse(KanyeResponse).GetValue("quote").ToString();
+                Console.WriteLine($"\nKanye: {KanyeQuote}");
+
+                var SwansonResponse = client.GetStringAsync(SwansonUrl).Result;
+                var ronQuote = JArray.Parse(SwansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+                Console.WriteLine($"\nSwanson: {ronQuote}");
+
+            }
+        }
+    }
+}
